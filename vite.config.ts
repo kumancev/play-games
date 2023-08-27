871ad64844f15ee3112f9b8dcf1a0badb44ec601
Alex Kumancev <kumancev1@gmail.com>
2023-08-27 15:43:58 +0300

    feat: add aliases for files path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001
-  }
+	plugins: [react()],
+	server: {
+		port: 3001,
+	},
+	resolve: {
+		alias: {
+			'@styles': path.resolve(__dirname, './src/styles'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@app': path.resolve(__dirname, './src/app'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@helpers': path.resolve(__dirname, './src/helpers'),
+		},
+	},
 })
