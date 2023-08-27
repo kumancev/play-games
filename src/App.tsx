a769181c372942e76e04600cba86c623e9c8ba3c
Alex Kumancev <kumancev1@gmail.com>
2023-08-27 14:45:23 +0300

    feat: add husky for check code before commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css';
+import './App.css'
 
 function App() {
 	return (
 		<>
 			<h1>Hello</h1>
-      <a href="https://site.com"></a>
 		</>
 	)
 }
 
-export default App;
+export default App
